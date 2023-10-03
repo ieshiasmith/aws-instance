7a9993b5ade682f553ef943530bb7fd34e762c93
Daniel Fedick <daniel.fedick@hashicorp.com>
2023-10-03 16:22:54 -0400

    Add hardcoded strings

---
+++ consul-k8s/Readme.md
@@ -0,0 +1,9 @@
+
+
+
+
+# Start Minikube
+
+```
+ minikube start --profile dc1 --memory 12288 --kubernetes-version=v1.22.0
+```
--- main.tf
+++ main.tf
@@ -14,13 +14,17 @@ data "aws_ami" "ubuntu_focal" {
 }
 
 locals {
-  public_subnet_0 = element(data.terraform_remote_state.vpc.outputs.public_subnet_ids, 0)
-  public_subnets  = data.terraform_remote_state.vpc.outputs.*.public_subnet_ids
-  vpc_id          = data.terraform_remote_state.vpc.outputs.vpc_id
+  # public_subnet_0 = element(data.terraform_remote_state.vpc.outputs.public_subnet_ids, 0)
+  # public_subnets  = data.terraform_remote_state.vpc.outputs.*.public_subnet_ids
+  # vpc_id          = data.terraform_remote_state.vpc.outputs.vpc_id
+  public_subnet_0 = ["subnet-05d98c13d2b22d16b"]
+  public_subnets  = ["subnet-05d98c13d2b22d16b", "subnet-0a63f11b5872f7be6"]
+  vpc_id          = "vpc-0e984b200b6f7844f"
   // This AMI is the Ubuntu 20.04 located in us-east-2
-  my_ip              = var.my_ip
-  cidr_block         = var.cidr_block
-  ami_id             = data.aws_ami.ubuntu_focal.image_id
+  my_ip      = var.my_ip
+  cidr_block = var.cidr_block
+  #  ami_id             = data.aws_ami.ubuntu_focal.image_id
+  ami_id             = "ami-01251fbaa37843d10"
   management_key     = "management"
   management_privkey = base64decode(var.management_privkey)
   ssh_sg             = aws_security_group.ssh_sg.id
