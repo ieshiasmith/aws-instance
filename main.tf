a1f3e804337919771eea309cdef3bf1ee8366f3b
Daniel Fedick <daniel.fedick@hashicorp.com>
2023-10-04 08:35:06 -0400

    Update to remote_state

--- main.tf
+++ main.tf
@@ -14,17 +14,17 @@ data "aws_ami" "ubuntu_focal" {
 }
 
 locals {
-  # public_subnet_0 = element(data.terraform_remote_state.vpc.outputs.public_subnet_ids, 0)
-  # public_subnets  = data.terraform_remote_state.vpc.outputs.*.public_subnet_ids
-  # vpc_id          = data.terraform_remote_state.vpc.outputs.vpc_id
-  public_subnet_0 = "subnet-0d2dfe2868e46e36d"
-  public_subnets  = ["subnet-0d2dfe2868e46e36d"]
-  vpc_id          = "vpc-0e984b200b6f7844f"
+  public_subnet_0 = element(data.terraform_remote_state.vpc.outputs.public_subnet_ids, 0)
+  public_subnets  = data.terraform_remote_state.vpc.outputs.*.public_subnet_ids
+  vpc_id          = data.terraform_remote_state.vpc.outputs.vpc_id
+  # public_subnet_0 = "subnet-0d2dfe2868e46e36d"
+  # public_subnets  = ["subnet-0d2dfe2868e46e36d"]
+  # vpc_id          = "vpc-0e984b200b6f7844f"
   // This AMI is the Ubuntu 20.04 located in us-east-2
   my_ip      = var.my_ip
   cidr_block = var.cidr_block
-  #  ami_id             = data.aws_ami.ubuntu_focal.image_id
-  ami_id             = "ami-01251fbaa37843d10"
+  ami_id     = data.aws_ami.ubuntu_focal.image_id
+  # ami_id             = "ami-01251fbaa37843d10"
   management_key     = "management"
   management_privkey = base64decode(var.management_privkey)
   ssh_sg             = aws_security_group.ssh_sg.id
